ed60489d4b2e13eb17dcf4fe52b9bb87823193fa
Grzeluu <patryk.grzelewski6@gmail.com>
2021-11-26 21:04:08 +0100

    Added pixels energy calculation

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,5 +1,19 @@
 package seamcarving
 
+import seamcarving.seamcarving.SeamCarving
+import java.io.File
+import javax.imageio.ImageIO
+
 fun main() {
-    
+    println("Type input image path")
+    val input = readLine()!!
+    println("Type output image path")
+    val output = readLine()!!
+    println("How many pixels in width would you reduce your image?")
+    val widthReduce = readLine()!!.toInt()
+    println("How many pixels in height would you reduce your image?")
+    val heightReduce = readLine()!!.toInt()
+
+    val image = ImageIO.read(File(input))
+    val resizedImage = SeamCarving(image).resize(widthReduce, heightReduce)
 }
